1e0a3826aefda40f4565f5caa1fa5fa7f45a12b9
chaitanya2404 <63045571+chaitanya2404@users.noreply.github.com>
2020-04-07 15:53:12 +0530

    Update osproject.cpp

--- osproject.cpp
+++ osproject.cpp
@@ -13,8 +13,7 @@ for(int i=0; i<num; i++) {
 for(int j=0; j<num-i-1; j++) {
  if(arr[j][1] > arr[j+1][1]) {
  for(int k=0; k<5; k++) {
- swap(arr[j][k], arr[j+1][k]);
-}
+ swap(arr[j][k], arr[j+1][k]);}
 }
 }
 }
